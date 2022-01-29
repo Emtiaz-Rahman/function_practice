1a115b7a94e0dc75c2779884005ff6aabd785884
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-29 15:25:39 +0600

    multiple

---
+++ multiple-parameter.js
@@ -0,0 +1,24 @@
+
+function addTwonmber(number1, number2) {
+   var total = number1 + number2;
+   return total;
+}
+
+var first1 = addTwonmber(10, 20);
+
+
+console.log("Total sum  = "  + first1);
+
+var multiplyTwo = first1 * 2;
+console.log("Multipluy Value = " + multiplyTwo)
+
+function addTwonmber(number1, number2) {
+    var total = number1 + number2;
+    return total;
+}
+var first1 = addTwonmber(25, 35);
+
+console.log("Total sum=" + first1);
+var difference = first1 - 2;
+console.log('Difference=' + difference);
+

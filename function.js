e50e9ad88bc7893aeee390f2c1135a0f082b9bc9
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-29 10:52:21 +0600

    function

---
+++ function.js
@@ -0,0 +1,8 @@
+function startFan() {
+    console.log('walk towards the fan');
+    console.log('press the switch');
+}
+startFan()
+console.log('I go to market');
+console.log('I eat rice');
+startFan()

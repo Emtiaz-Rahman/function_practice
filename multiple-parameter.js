1189c3099e1dc1ff88ff957c4c3bbef96e6379c5
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-29 15:40:10 +0600

    exercise

--- multiple-parameter.js
+++ multiple-parameter.js
@@ -1,24 +1,32 @@
 
-function addTwonmber(number1, number2) {
-   var total = number1 + number2;
-   return total;
+// function addTwonmber(number1, number2) {
+//    var total = number1 + number2;
+//    return total;
+// }
+
+// var first1 = addTwonmber(10, 20);
+
+
+// console.log("Total sum  = "  + first1);
+
+// var multiplyTwo = first1 * 2;
+// console.log("Multipluy Value = " + multiplyTwo)
+
+// function addTwonmber(number1, number2) {
+//     var total = number1 + number2;
+//     return total;
+// }
+// var first1 = addTwonmber(25, 35);
+
+// console.log("Total sum=" + first1);
+// var difference = first1 - 2;
+// console.log('Difference=' + difference);
+
+function subjectsMarks(subject1, subject2, subject3, subject4, subject5) {
+    var totalMarks = subject1 + subject2 + subject3 + subject4 + subject5;
+    return totalMarks;
 }
-
-var first1 = addTwonmber(10, 20);
-
-
-console.log("Total sum  = "  + first1);
-
-var multiplyTwo = first1 * 2;
-console.log("Multipluy Value = " + multiplyTwo)
-
-function addTwonmber(number1, number2) {
-    var total = number1 + number2;
-    return total;
-}
-var first1 = addTwonmber(25, 35);
-
-console.log("Total sum=" + first1);
-var difference = first1 - 2;
-console.log('Difference=' + difference);
-
+var total = subjectsMarks(50, 60, 80, 94, 64);
+console.log('totalMarks=' + total);
+var average = total / 5;
+console.log('Average Mraks=' + average);
